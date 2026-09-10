2455f2c9dd262686aca90701df918c847d5916b6
luckyk3772-lab <luckyk3772@gmail.com>
2026-09-10 20:01:31 +0530

    Time: 5 ms (82.52%), Space: 45.8 MB (86.61%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+    int ans =0;
+    int temp = x;
+        if (x<0){
+            return false;
+        }
+       while(x != 0){
+        ans = (ans * 10) + x % 10;
+        x = x/10;
+       }
+        return ans == temp; 
+    }
+}
